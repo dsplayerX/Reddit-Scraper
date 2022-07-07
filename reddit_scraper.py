09c155f93c353ec610e49cb1ded1c9276f986ada
dsplayerX <dumindu0043@gmail.com>
2022-07-07 12:07:10 +0530

    completed image scraping

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -20,31 +20,43 @@ def initReddit():
         password="",
         )
 
-def scrapeImages(sub_name, sub_sort, scrape_limit):
 
-    subreddit = reddit.subreddit(sub_name)
-
-    for submission in subreddit.hot(limit = scrape_limit):
-        print(submission.title)
-        print(submission.url)
-        print()
-        if "jpg" in submission.url.lower() or "png" in submission.url.lower():
-            print("Image found")
-            image = requests.get(submission.url)
-            file = open("images/" + sub_name + "-" + submission.id + ".png", "wb")
-            file.write(image.content)
-            file.close()
-
-
-def scrapeVideos(reddit):
-    permaurl = "https://www.reddit.com/"+submission.permalink
-    file = RedDownloader.Download(url = permaurl , output=subreddit_name + "-" + submission.id , destination="videos/" , quality = 360)
-    print(permaurl)
-
-        
 def scrapePosts(reddit):
     pass        
 
+
+def scrapeImages(sub_name, sub_sort, scrape_limit):
+
+    subreddit = reddit.subreddit(sub_name)
+  
+    saveCount = 0
+
+    print(f" > Scraping {scrape_limit} posts from r/{sub_name} for images...")
+
+    for submission in getSortedSubreddit(subreddit, sub_sort)(limit = scrape_limit):
+        if "jpg" in submission.url.lower() or "png" in submission.url.lower():
+            image = requests.get(submission.url)
+            file = open("images/" + sub_name + "-" + submission.title[:15] + "-" + submission.id + ".png", "wb")
+            file.write(image.content)
+            file.close()
+            saveCount += 1
+    print(f" > Saved {saveCount} image(s).")
+
+
+def scrapeVideos(sub_name, sub_sort, scrape_limit, quality):
+
+    subreddit = reddit.subreddit(sub_name)
+
+    saveCount = 0
+
+    print(f" > Scraping {scrape_limit} posts from r/{sub_name} for videos...")
+
+    for submission in getSortedSubreddit(subreddit, sub_sort)(limit = scrape_limit):
+        permaURL = "https://www.reddit.com/"+submission.permalink
+        file = RedDownloader.Download(url = permaURL , output=sub_name + "-" + submission.id , destination="videos/" , quality = quality)
+        print(permaURL)
+    print(f" > Saved {saveCount} video(s).")
+        
 def menu():
     print("Reddit Scraper")
     print("1. Post URLs")
@@ -53,6 +65,28 @@ def menu():
     print("q. Quit")
     userChoice = input("What do you want to scrape? ")
     return userChoice
+
+
+def printSortMethods():
+    print(" 1.controversial")
+    print(" 2.gilded")
+    print(" 3.hot")
+    print(" 4.new")
+    print(" 5.rising")
+    print(" 6.top of all time")
+
+    
+def getSortedSubreddit(subreddit, sort):
+    sub_sort_dict = {
+        1 : subreddit.controversial,
+        2 : subreddit.gilded,
+        3 : subreddit.hot,
+        4 : subreddit.new,
+        5 : subreddit.rising,
+        6 : subreddit.top
+    }
+    return sub_sort_dict[sort]
+
 
 def main():
     
@@ -64,12 +98,29 @@ def main():
         userChoice = menu()
         if userChoice == "1":
             pass
+
         elif userChoice == "2":
-            scrapeImages("aww", "top", 2)
+            userSub = input("Enter subreddit name: ")
+            printSortMethods()
+            userSort = int(input("Enter sort method: "))
+            userLimit = int(input("How many posts to scrape: "))
+            scrapeImages(userSub, userSort, userLimit)
+
         elif userChoice == "3":
-            pass
+            userSub = input("Enter subreddit name: ")
+            printSortMethods()
+            userSort = int(input("Enter sort method: "))
+            userLimit = int(input("How many posts to scrape: "))
+
+            print("Avaliable options to choose from are 144, 240, 360, 480, 720, 1080")
+            print("If a video file in specified resolution is not found it will try for a lower resolution")
+
+            userQuality = int(input("Enter video quality: "))
+            scrapeVideos(userSub, userSort, userLimit, userQuality)
+
         elif userChoice.lower() == "q":
             isRunning = False
+
         else:
             print("Wrong input! Try again!")
 
